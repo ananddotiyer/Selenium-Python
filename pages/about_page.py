9371ee3cfff1fd4104477e083f571ac3fbf7e8a0
HappyWaters <ananddotiyer@gmail.com>
2020-02-14 09:34:05 +0530

    Updated

--- pages/about_page.py
+++ pages/about_page.py
@@ -1,29 +1,15 @@
-#!/usr/bin/env python
-#############################################################################################################################################
-__filename__ = "about_page.py"
-__description__ = """Represents about page
-
+"""
 Methods that launch new pages start with get_
 Methods that are used for verification/assertion purposes start with is_
 Method that perform actions start with do_
-Method that perform verifications start with verify_
-
+Method that perform actions start with verify_
 """
-
-__author__ = "Anand Iyer"
-__copyright__ = "Copyright 2017"
-__credits__ = ["Anand Iyer"]
-__version__ = "1.0"
-__maintainer__ = "Anand Iyer"
-__email__ = "ananddotiyer@gmail.com"
-__status__ = "Testing" # Upgrade to Production once tested to function.
-#############################################################################################################################################
 
 import time
 from common.selenium_wrappers import *
 from common.common import *
 from common.config import *
-from a_base_page import basePage
+from pages.a_base_page import basePage
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
@@ -33,13 +19,13 @@ class aboutPageLocators(object):
     ARCHIVES = (By.XPATH, "//a[@title='Archives']")
 
     PAGE_SYNC = ARCHIVES
-    
+
 class aboutPage(basePage):
     def __init__(self, driver):
         super(aboutPage, self).__init__(driver)
         WebDriverWait(driver, 100).until(
             lambda driver: driver.find_element(*aboutPageLocators.PAGE_SYNC))
-        
+
     def get_archives (self):
         get_archives = self.driver.find_element(*aboutPageLocators.ARCHIVES)
         click (get_archives, wait_least)
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,29 +1,15 @@
-#!/usr/bin/env python
-#############################################################################################################################################
-__filename__ = "main_page.py"
-__description__ = """Represents main page
-
+"""
 Methods that launch new pages start with get_
 Methods that are used for verification/assertion purposes start with is_
 Method that perform actions start with do_
-Method that perform verifications start with verify_
-
+Method that perform actions start with verify_
 """
-
-__author__ = "Anand Iyer"
-__copyright__ = "Copyright 2017"
-__credits__ = ["Anand Iyer"]
-__version__ = "1.0"
-__maintainer__ = "Anand Iyer"
-__email__ = "ananddotiyer@gmail.com"
-__status__ = "Testing" # Upgrade to Production once tested to function.
-#############################################################################################################################################
 
 import time
 from common.selenium_wrappers import *
 from common.common import *
 from common.config import *
-from a_base_page import basePage
+from pages.a_base_page import basePage
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
@@ -33,13 +19,13 @@ class mainPageLocators(object):
     ABOUT = (By.XPATH, "//a[@title='About']")
 
     PAGE_SYNC = ABOUT
-    
+
 class mainPage(basePage):
     def __init__(self, driver):
         super(mainPage, self).__init__(driver)
         WebDriverWait(driver, 100).until(
             lambda driver: driver.find_element(*mainPageLocators.PAGE_SYNC))
-        
+
     def get_about (self):
         get_about = self.driver.find_element(*mainPageLocators.ABOUT)
         click (get_about, wait_least)
